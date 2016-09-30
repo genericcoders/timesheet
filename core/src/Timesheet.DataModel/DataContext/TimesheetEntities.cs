a88cac01d08f6b2abb105e2c6bbf636dbd82ce0a
Nathan Ratliff <nathan.ratliff@lightmaker.com>
2016-09-30 16:11:00 -0400

    Updated using spaces for TimesheetEntities and removed unused classes.

--- core/src/Timesheet.DataModel/DataContext/TimesheetEntities.cs
+++ core/src/Timesheet.DataModel/DataContext/TimesheetEntities.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Timesheet.Service.Models;
+using Timesheet.Entities.Models;
 
 namespace Timesheet.Entities
 {
