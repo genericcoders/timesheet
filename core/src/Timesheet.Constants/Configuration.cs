dec391fafc419ea21f007f436189a4b2518ddacc
Nathan Ratliff <nathan.ratliff@lightmaker.com>
2016-10-03 12:00:52 -0400

    Added Timesheet.Constants for using a ConnectionString temporarily. Need to change this to a per environment basis.

---
+++ core/src/Timesheet.Constants/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheet.Constants
+{
+	public class Configuration
+	{
+		public const string ConnectionString =
+			"Server=TERMINAL;Database=Timesheet;Trusted_Connection=true;MultipleActiveResultSets=true;";
+	}
+}
