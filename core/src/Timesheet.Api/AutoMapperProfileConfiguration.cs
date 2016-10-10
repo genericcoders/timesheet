b5bf7d165c06afa5e80c4dd6c4d7aabacb044bbb
Nathan Ratliff <nathan.ratliff@lightmaker.com>
2016-10-09 23:47:34 -0400

    Added nugget packages and references for Automapper and Generic repositories Added new DI for automapper and repositories

---
+++ core/src/Timesheet.Api/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Timesheet.Entities.Models;
+using Timesheet.Dto;
+
+namespace Timesheet.Api
+{
+    public class AutoMapperProfileConfiguration : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<EmployeeEntity, EmployeeDto>();
+            CreateMap<EmployeeDto, EmployeeEntity>();
+        }
+    }
+}
