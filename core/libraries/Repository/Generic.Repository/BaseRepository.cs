9dccdbfabdfeaeaf4e0f07a92d58f0d9f7aa2d4b
Nathan Ratliff <nathan.ratliff@lightmaker.com>
2016-10-09 23:30:54 -0400

    Added a Generic.Repository to handle basic CRUD operations so that the Service layer can handle the business logic

---
+++ core/libraries/Repository/Generic.Repository/BaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Generic.Repository.Interfaces;
+
+namespace Generic.Repository
+{
+    public class BaseRepository<TEntity, TPrimKey> : IRepository<TEntity, TPrimKey> where TEntity : class//where TEntity : class, IRepository<TEntity, TPrimKey> 
+    {
+        private readonly IApplicationEntities _context;
+
+        public BaseRepository(IApplicationEntities context)
+        {
+            _context = context; 
+        }
+
+        public bool Create(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(TPrimKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        public TEntity Get(TPrimKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
