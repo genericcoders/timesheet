e88a774dff542448a6d6f8c6ae89507b612294bf
Nathan Ratliff <nathan.ratliff@lightmaker.com>
2016-10-09 23:42:45 -0400

    Added the Timesheet.Repository to handle the Generic.Repository when it is passed to the service layer.

---
+++ core/Timesheet.Repository/EmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using Generic.Repository;
+using Generic.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Timesheet.Entities.Models;
+
+namespace Timesheet.Repository
+{
+    public class EmployeeRepository : BaseRepository<EmployeeEntity, int>
+    {
+
+        private readonly IApplicationEntities _context;
+
+        public EmployeeRepository(IApplicationEntities context)
+            : base(context)
+        {
+            _context = context;
+        }
+    }
+
+}
