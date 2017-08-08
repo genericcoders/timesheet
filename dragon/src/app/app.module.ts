0d1fafa4befa2b2a75cf0d056145a2340a8661c0
Jason Shih <jasonshihprofessional@gmail.com>
2017-08-08 00:57:08 -0400

    TopNav: Generated

--- dragon/src/app/app.module.ts
+++ dragon/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule
