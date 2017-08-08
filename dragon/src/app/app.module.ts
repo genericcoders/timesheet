3f304665c2ad0d31b916f42fb5d03a8d245082c6
Jason Shih <jasonshihprofessional@gmail.com>
2017-08-08 04:14:03 -0400

    TopNav: Added in Directive for DOM control

--- dragon/src/app/app.module.ts
+++ dragon/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive'
 import { UserComponent } from './components/user/user.component';
 import { TopnavComponent } from './components/topnav/topnav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HighlightDirective,
     UserComponent,
     TopnavComponent
   ],
