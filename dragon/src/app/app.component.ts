2db35434610443d00e8b5dd0cc78fe02c6583e0c
Jason Shih <jasonshihprofessional@gmail.com>
2017-08-07 20:26:31 -0400

    Basic Restructure

--- dragon/src/app/app.component.ts
+++ dragon/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dr-index',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dragon Scheduler';
+  title = 'Dragon';
 }
